7fea3d36703d175be7a7b6a7a6ba104b2ce63d6d
Tatsiana Klybik <t.klybik@godeltech.com>
2025-04-22 19:47:14 +0200

    Add global config vars to e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,3 +15,12 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
+import l10n from './l10n.json';
+import reqs from './requirements';
+import urls from './urls';
+import selectors from './selectors';
+
+Cypress.env('l10n', l10n);
+Cypress.env('reqs', reqs);
+Cypress.env('urls', urls);
+Cypress.env('selectors', selectors);
