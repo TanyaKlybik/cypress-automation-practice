1ce12c6424e0a46160b96c399873c015d96a87af
Tatsiana Klybik <t.klybik@godeltech.com>
2025-04-22 11:09:55 +0200

    Configure Cypress: set base URL and dev env

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,16 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://www.saucedemo.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      console.log('Running tests in:', config.env.environment || 'not set');
+      return config;
     },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.js',
+  },
+  env: {
+    environment: 'development',
   },
 });
